f57909c071ffd681788eced69286c33d6d72fc31
bbvch13531 <bbvch13531@gmail.com>
2018-08-31 22:13:27 +0900

    Initial commit
    Signed-off-by: bbvch13531 <bbvch13531@gmail.com>

---
+++ ImageExportExtension/ImageExportExtension/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ImageExportExtension
+//
+//  Created by KyungYoung Heo on 2018. 8. 31..
+//  Copyright © 2018년 KyungYoung Heo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
