ef04ea16e26598ebb475ad7d65446b918e71afaa
bbvch13531 <bbvch13531@gmail.com>
2018-09-03 10:58:39 +0900

    Add extension to project
    Signed-off-by: bbvch13531 <bbvch13531@gmail.com>

--- ImageExportExtension/ImageExport/ShareViewController.swift
+++ ImageExportExtension/ImageExport/ShareViewController.swift
@@ -20,7 +20,9 @@ class ShareViewController: SLComposeServiceViewController {
         // This is called after the user selects Post. Do the upload of contentText and/or NSExtensionContext attachments.
     
         // Inform the host that we're done, so it un-blocks its UI. Note: Alternatively you could call super's -didSelectPost, which will similarly complete the extension context.
+		
         self.extensionContext!.completeRequest(returningItems: [], completionHandler: nil)
+		
     }
 
     override func configurationItems() -> [Any]! {
