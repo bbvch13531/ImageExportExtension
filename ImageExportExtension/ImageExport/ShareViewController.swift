dc5bd3956d962bcd03a264d19f2755f812084fc8
bbvch13531 <bbvch13531@gmail.com>
2018-09-05 18:33:54 +0900

    Add get image code

--- ImageExportExtension/ImageExport/ShareViewController.swift
+++ ImageExportExtension/ImageExport/ShareViewController.swift
@@ -8,18 +8,84 @@
 
 import UIKit
 import Social
+import CoreData
+
 
 class ShareViewController: SLComposeServiceViewController {
 
+	let container = NSPersistentContainer(name: "DataModel")
+	
+	override func viewDidLoad() {
+		
+		
+		container.loadPersistentStores(completionHandler: { (description, error) in
+			if let error = error {
+				fatalError("Unable to load persistent stores: \(error)")
+			}
+		})
+	}
+	
     override func isContentValid() -> Bool {
         // Do validation of contentText and/or NSExtensionContext attachments here
         return true
     }
-
+	
     override func didSelectPost() {
         // This is called after the user selects Post. Do the upload of contentText and/or NSExtensionContext attachments.
     
         // Inform the host that we're done, so it un-blocks its UI. Note: Alternatively you could call super's -didSelectPost, which will similarly complete the extension context.
+		let fileManager = FileManager()
+		
+		
+		if let item = self.extensionContext?.inputItems[0] as? NSExtensionItem {
+//			print(item.attachments)
+			for ele in item.attachments!{
+			
+				let itemProvider = ele as! NSItemProvider
+				
+				if itemProvider.hasItemConformingToTypeIdentifier("public.jpg"){
+					print(itemProvider)
+					itemProvider.loadItem(forTypeIdentifier: "public.jpg", options: nil, completionHandler: { (item, error) in
+//						do {
+//							var imgData: Data!
+//							if let url = item as? URL{
+//								imgData = try Data(contentsOf: url)
+//							}
+//
+//							if let img = item as? UIImage{
+//								imgData = UIImagePNGRepresentation(img)
+//							}
+//
+//							let dict: [String : Any] = ["imgData" :  imgData, "name" : self.contentText]
+//							print("\n\n\n\(dict)")
+//							let userDefault = UserDefaults(suiteName: "group.imageExportor")
+//							userDefault?.set(dict, forKey: "img")
+//							userDefault?.synchronize()
+//
+//						}catch let err{
+//							print(err)
+//						}
+						do{
+							var imgData: Data!
+							if let url = item as? URL{
+								imgData = try Data(contentsOf: url)
+							}
+
+							if let img = item as? UIImage{
+								imgData = UIImagePNGRepresentation(img)
+							}
+							print("imgData =  \(imgData)")
+						} catch let err{
+							print(err)
+						}
+					})
+				
+				}
+				else	{
+					print("Cannot find image")
+				}
+			}
+		}
 		
         self.extensionContext!.completeRequest(returningItems: [], completionHandler: nil)
 		
