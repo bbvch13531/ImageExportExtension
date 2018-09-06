c022def180748556203650a2062663cba203eac2
bbvch13531 <bbvch13531@gmail.com>
2018-09-06 21:57:57 +0900

    Upload image on server by post

--- ImageExportExtension/ImageExport/ShareViewController.swift
+++ ImageExportExtension/ImageExport/ShareViewController.swift
@@ -9,20 +9,12 @@
 import UIKit
 import Social
 import CoreData
-
+import Alamofire
 
 class ShareViewController: SLComposeServiceViewController {
-
-	let container = NSPersistentContainer(name: "DataModel")
 	
 	override func viewDidLoad() {
 		
-		
-		container.loadPersistentStores(completionHandler: { (description, error) in
-			if let error = error {
-				fatalError("Unable to load persistent stores: \(error)")
-			}
-		})
 	}
 	
     override func isContentValid() -> Bool {
@@ -34,8 +26,6 @@ class ShareViewController: SLComposeServiceViewController {
         // This is called after the user selects Post. Do the upload of contentText and/or NSExtensionContext attachments.
     
         // Inform the host that we're done, so it un-blocks its UI. Note: Alternatively you could call super's -didSelectPost, which will similarly complete the extension context.
-		let fileManager = FileManager.default
-		
 		
 		if let item = self.extensionContext?.inputItems[0] as? NSExtensionItem {
 //			print(item.attachments)
@@ -46,7 +36,7 @@ class ShareViewController: SLComposeServiceViewController {
 				
 				if itemProvider.hasItemConformingToTypeIdentifier("public.png"){
 					print(itemProvider)
-//					itemProvider.loadItem(forTypeIdentifier: "public.jpg", options: nil, completionHandler: { (item, error) in
+//					itemProvider.loadItem(forTypeIdentifier: "public.jpeg", options: nil, completionHandler: { (item, error) in
 //						do{
 //							var imgData: Data!
 //							if let url = item as? URL{
@@ -63,8 +53,29 @@ class ShareViewController: SLComposeServiceViewController {
 //					})
 					itemProvider.loadItem(forTypeIdentifier: "public.png", options: nil, completionHandler: {(item,error) in
 						do{
-							print("found image : \(item)")
-							Alamofire.request
+							var imgData = UIImage()
+//							if let data = item {
+//								imgData = UIImage(data: NSData(contentsOfURL: data as! NSURL)!)
+//							}
+							if let url = item as? NSURL{
+								if let data = NSData(contentsOf: url as URL){
+									imgData = UIImage(data: data as Data)!
+//									self.uploadImage(data: data as Data)
+								}
+							}
+//							if let img = item as? UIImage{
+//								imgData = UIImagePNGRepresentation(img)
+//
+//							} else {
+//
+//							}
+							if let uploadImgData = UIImagePNGRepresentation(imgData) {
+								
+								self.uploadImage(data: uploadImgData)
+							}
+							
+							print("imgData = \(imgData), item = \(item)")
+
 						} catch let error {
 							print(error)
 						}
@@ -75,14 +86,41 @@ class ShareViewController: SLComposeServiceViewController {
 				}
 			}
 		}
-		
         self.extensionContext!.completeRequest(returningItems: [], completionHandler: nil)
-		
     }
 
     override func configurationItems() -> [Any]! {
         // To add configuration options via table cells at the bottom of the sheet, return an array of SLComposeSheetConfigurationItem here.
         return []
     }
-
+	func uploadImage(data:Data){
+		var params: Parameters = [
+			"file" : data
+		]
+//		let encoding = URLEncoding.httpBody
+//		Alamofire.request("http://neinsys.io:5000/insertImagePost", method: .post, parameters: params, encoding: encoding)
+//			.validate(contentType: ["multipart/form-data"])
+//			.response { response in
+////			print(response.result)
+//			debugPrint(response)
+//		}
+		print("data = \(data)")
+		Alamofire.upload(
+			multipartFormData: { multipartFormData in
+				multipartFormData.append(data, withName:"file", fileName: "extension.jpg", mimeType: "image/jpg")
+			},
+			to : "http://neinsys.io:5000/insertImagePost",
+			encodingCompletion: { encodingResult in
+				switch encodingResult {
+				case .success(let upload, _, _):
+					upload.responseString { response in
+						debugPrint(response.result.value)
+					}
+					print("success")
+				case .failure(let encodingError):
+					print("fail! \(encodingError)")
+				}
+			}
+		)
+	}
 }
--- ImageExportExtension/ImageExportExtension/ViewController.swift
+++ ImageExportExtension/ImageExportExtension/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+	@IBOutlet weak var imgView: UIImageView!
+	@IBOutlet weak var lblText: UILabel!
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +22,24 @@ class ViewController: UIViewController {
 		super.didReceiveMemoryWarning()
 		// Dispose of any resources that can be recreated.
 	}
-
+	override func viewWillAppear(_ animated: Bool) {
+		super.viewWillAppear(animated)
+		
+		let userDefault = UserDefaults.standard
+		userDefault.addSuite(named: "group.ImageExportor")
+		
+		if let dict = userDefault.value(forKey: "img") as? NSDictionary{
+			
+			let data = dict.value(forKey: "imgData") as! Data
+			let str = dict.value(forKey: "name") as! String
+			
+			self.imgView.image = UIImage(data: data)
+			self.lblText.text = str
+			
+			userDefault.removeObject(forKey: "img")
+			userDefault.synchronize()
+		}
+	}
 
 }
 
